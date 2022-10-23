66c132449579c992bc47123101f9ac74f8009735
Victor Huang <victorhuangwq@gmail.com>
2022-10-23 12:39:50 -0400

    Add file stubs for potential classes

--- match_runner.py
+++ match_runner.py
@@ -9,5 +9,10 @@ class MatchRunner:
     def sendJob(self):
         """
         Send the job to the match runner by calling Tango API
+        You would likely need to download the user submissions from the remote location
+        and then send it together with the game engine to the match runner
+
+        You will likely need the requests library to call the Tango API
+        Tango API https://docs.autolabproject.com/tango-rest/
         """
         raise NotImplementedError
