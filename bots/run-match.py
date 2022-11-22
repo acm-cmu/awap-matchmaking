1e534760dd4cf9429e78d1d861574fe84492794b
Lhwhatever <llinhong.2301+github@gmail.com>
2022-11-22 14:06:28 -0500

    Rename set of bots

--- bots/run-match.py
+++ bots/run-match.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     team2_score = team2.main()
     print(f"Team 2 score = {team2_score}")
 
+    assert team1_score > 0
+    assert team2_score > 0
+
     outcome = 1 if team1_score > team2_score else 2
     score = {"scores": {"Team1": team1_score, "Team2": team2_score, "Outcome": outcome}}
     print(json.dumps(score))
