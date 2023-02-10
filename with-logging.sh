6b86d7aa6c926205688d85dfd3642b2a284d4912
Lhwhatever <llinhong.2301+github@gmail.com>
2023-02-10 15:59:34 -0500

    Add withlogging

---
+++ with-logging.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mkdir -p logs
+DATE=$(date -Iseconds)
+STDOUT_LOG="logs/stdout-$DATE.log"
+STDERR_LOG="logs/stderr-$DATE.log"
+touch $STDOUT_LOG $STDERR_LOG
+ln -f -s $PWD/$STDOUT_LOG $PWD/logs/stdout-latest.log
+ln -f -s $PWD/$STDERR_LOG $PWD/logs/stderr-latest.log
+echo $@
+$@ > $STDOUT_LOG 2> $STDERR_LOG
